37538d1bfa02060111afbe31c89f90d37fd62087
Tjørn Lynghus <tjornlyng@gmail.com>
2023-05-16 04:33:25 +0200

    accuracy_script better. Use makefile on server.

--- code/accuracy_script.sh
+++ code/accuracy_script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-
-for (( c=5; c<=20; c +=5 ))
+for i in  20; do
+for (( c=5; c<=20; c +=5 ));
 do
    i64s="i64"
    i32s="i32"
-   var_ktmp="-g $1$i32s -g $c$i64s -g [10000][50]f32 -g [500][50]f32"
+   var_ktmp="-b -g $i$i32s -g $c$i64s -g [100000][150]f32 -g [500][150]f32"
    futhark dataset $var_ktmp > tmpdata$c
-   echo "Data set with k = $c created. Acc for configuration of RANN is "
+   echo "Data set with k = $c , t = $i created. Acc for configuration of RANN is "
     ./knn-comparer -t time_with_k$c < tmpdata$c
-done
-
-var_ktmp="-g $1$i32s -g 100$i64s -g [10000][50]f32 -g [500][50]f32"
+done;
+var_ktmp="-b -g $i$i32s -g 100$i64s -g [100000][150]f32 -g [500][150]f32"
 futhark dataset $var_ktmp > tmpdata100
-echo "Data set with k = 100 created. Acc for configuration of RANN is "
+echo "Data set with k = 100, t = $i created. Acc for configuration of RANN is "
 ./knn-comparer -t time_with_k100 < tmpdata100
+done
